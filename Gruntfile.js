3d7066e6e685272b9a43140cc740a13fab76ad45
wagnermoschini <wagnermoschini@gmail.com>
2014-02-04 20:02:27 -0200

    implementing stylus configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,57 +1,70 @@
 module.exports = function(grunt) {
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    proj: {
+        // Configurable paths
+        app:  'app',
+        dist: 'dist'
+    },
+
     concat: {
-	  options: {
-	    separator: ';'
-	  },
-	  dist: {
-	    src: ['js/plugins.js','js/application.js'],
-	    dest: 'js/main.js'
-	  }
-	},
+  	  options: {
+  	    separator: ';'
+  	  },
+  	  dist: {
+  	    src: ['js/plugins.js','js/application.js'],
+  	    dest: 'js/main.js'
+  	  }
+  	},
 
-	uglify: {
-	  dist: {
-	    files: {
-	      'js/main.min.js': ['<%= concat.dist.dest %>']
-	    }
-	  }
-	},
+  	uglify: {
+  	  dist: {
+  	    files: {
+  	      'js/main.min.js': ['<%= concat.dist.dest %>']
+  	    }
+  	  }
+  	},
 
-	compass: {
-	 	dist:{
-	 		options: {              
-				raw: 'preferred_syntax = :sass\n',
-				relativeAssets: true,
-				sassDir: 'css/sass',
-				cssDir: 'css',
-				imagesDir: 'img',
-				outputStyle: 'compressed'
-			}	
-	 	}
-	},
-
-	watch: {
-        scripts: {
-            files: 'js/**/*',
-            tasks: ['concat','uglify']
+    stylus: {
+      compile: {
+        options: {
+          // paths: ['path/to/import', 'another/to/import'],
+          // urlfunc: 'embedurl', // use embedurl('test.png') in our code to trigger Data URI embedding
+          // use: [
+          //   require('fluidity') // use stylus plugin at compile time
+          // ],
+          // import: [      //  @import 'foo', 'bar/moo', etc. into every .styl file
+          //   'foo',       //  that is compiled. These might be findable based on values you gave
+          //   'bar/moo'    //  to `paths`, or a plugin you added under `use`
+          // ]
         },
-        styles: {
-            files: 'css/**/*',
-            tasks: ['compass']
+        files: {
+          'css/main.css': 'css/main.styl' // 1:1 compile
+          // 'path/to/another.css': ['path/to/sources/*.styl', 'path/to/more/*.styl'] // compile and concat into single file
         }
+      }
+    },
+
+  	watch: {
+      scripts: {
+          files: 'js/**/*',
+          tasks: ['concat','uglify']
+      },
+      styles: {
+          files: 'css/**/*',
+          tasks: ['stylus']
+      }
     }
 
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-stylus');
 
-  
-  grunt.registerTask('default', ['concat', 'uglify', 'compass']);
-  //grunt.registerTask('default', ['compass']);
+
+    grunt.registerTask('default', ['concat', 'uglify', 'stylus']);
 };
